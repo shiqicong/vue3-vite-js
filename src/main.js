45e8350ce7c0cb5020bb13dab36c83bd25dcd871
石奇聪 <shiqicong@mininglamp.com>
2022-06-16 15:55:09 +0800

    feat:eslint

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 
 import { createApp } from 'vue'
+import Element3 from 'element3'
+import 'element3/lib/theme-chalk/index.css'
 import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
@@ -7,4 +9,5 @@ const app = createApp(App)
 
 app.use(store)
     .use(router)
+    .use(Element3)
     .mount('#app')
